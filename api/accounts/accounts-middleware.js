c8aef11f548769abab5f3faad1b6a280c6d0584b
Inderpreet <inderpreet2004123@gmail.com>
2025-10-22 11:13:04 -0400

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,33 +1,44 @@
 const Account = require ('./accounts-model')
-
+const db = require ('../../data/db-config')
 
 exports.checkAccountPayload = (req, res, next) => {
   const error = {status : 404}
   const {name,budget} = req.body
   if (name === undefined || budget === undefined) {
     error.message = 'name and budget are required'
-    next(error) 
   } else if (typeof name !== 'string') {
     error.message = 'name of account must be a string'
-    next(error) 
   } else if (name.trim().length < 3 || name.trim().length > 100) {
     error.message = 'name of account must be between 3 and 100'
-    next(error) 
   } else if (typeof budget !== 'number' || isNaN(budget)) {
     error.message = 'budget of account must be a number'
-    next(error)
   } else if (budget < 0 || budget > 1000000) {
     error.message = 'budget of account is too large or too small'
-    next(error)
-  } else next()
+  } if (error.message) {
+   next(error)
+} else {
+  next()
+}
 }
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
 
-exports.checkAccountNameUnique = (req, res, next) => {
+exports.checkAccountNameUnique = async (req, res, next) => {
   // DO YOUR MAGIC
   console.log('checkAccountNameUnique middleware')
+  try {
+    const existing = await  db ('accounts')
+    .where('name', req.body.name.trim())
+    .first()
+
+    if (existing) {
+      next ({status: 400, message: 'that  name is taken'})
+    } else {
+      next()
+    }
+  } catch (err) {
   next()
+}
 }
 
 exports.checkAccountId = async (req, res, next) => {
